8267bde2f9d231f2d9d8558cb6699f82c73e9d0f
kedizhou <312869331@qq.com>
2015-06-29 10:05:42 +0800

    bash shell
    clean log file.

---
+++ digui.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+source /etc/profile
+export PATH=$PATH:/usr/bin/:/sbin/:/usr/sbin/
+t=22
+function l
+{
+	pd=$(cd $(dirname $0);pwd)
+	echo -e $(date): "\n$*" >> $pd/log.dat
+}
+
+function tEst
+{
+	if [ `echo "$(df -ThP /opt/logbak/ | grep -v '^F' | awk  '{print $(NF-1)}' | sed 's/%//')>${t}" | bc -l` -eq 1 ];then
+		return 1
+	else
+		return 0
+	fi
+}
+
+function Rm
+{
+	#[ tEst -eq 0 ] && return 0
+	#[ $p -gt 5 ] && return 0
+	flist=$(find /opt/logbak/ -type f -mtime $p)
+	let p++
+	if [ ! -z "$flist" ];then
+		Rm
+		let p--
+		r=$(find /opt/logbak/ -type f -mtime $p)
+		l "$r"
+		\rm -rf $r
+		tEst
+		[ $? -eq 0 ] && exit
+	else
+		let p--
+		return 0
+	fi
+}
+
+p=0
+Rm
